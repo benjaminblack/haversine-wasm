81c9ea5032f52614ad5baf044296b0b029ee4b00
Benjamin Black <bblack@outsideinc.com>
2022-05-22 14:04:16 -0500

    top-level await not supported yet; export async function

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,19 @@
 import Module from "./haversine.js";
 
-const module = await Module();
+const wasmModule = Module().then((module) => {
+  const wasmHaversine = module.cwrap("haversine", "double", [
+    "double",
+    "double",
+    "double",
+    "double",
+  ]);
 
-const haversine = module.cwrap("haversine", "double", [
-  "double",
-  "double",
-  "double",
-  "double",
-]);
+  return { wasmHaversine };
+});
+
+async function haversine(th1, ph1, th2, ph2) {
+  const { wasmHaversine } = await wasmModule;
+  return wasmHaversine(th1, ph1, th2, ph2);
+}
 
 export { haversine };
